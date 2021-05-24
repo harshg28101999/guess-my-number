dfe248f5eec25a98a57f3ebaab2efd2fafc98ee4
harshg28101999 <hmg5265@psu.edu>
2021-05-25 01:55:50 +0530

    First instructions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// DOM Manipulation
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 2;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!!';
+  }
+});
