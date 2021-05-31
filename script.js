44dc479bf742374224c21fefd1f6f7db37a22710
harshg28101999 <hmg5265@psu.edu>
2021-06-01 00:57:26 +0530

    Refactored Logic

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ let number = Math.trunc(Math.random()*20)+1
 let score = 20;
 let highScore = 0
 
+// Function for calling DOM on the message
+const displayMessage = function(message){
+  document.querySelector('.message').textContent = message
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number!!';
+    displayMessage ('No Number!!')
   }
 
   // When player wins
   else if (guess === number){
-    document.querySelector('.message').textContent = 'Correct Guess!';
+    displayMessage('Correct Guess!!')
     document.querySelector('.number').textContent = number;
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
@@ -34,7 +39,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score
     }
     else {
-      document.querySelector('.message').textContent = 'You Loose!!';
+      displayMessage('You Loose!!')
       document.querySelector('body').style.backgroundColor = '#FF0000'
       document.querySelector('.score').textContent = 0;
     }
@@ -49,7 +54,7 @@ document.querySelector('.again').addEventListener('click',function(){
   number = Math.trunc(Math.random()*20)+1
 
   document.querySelector('body').style.backgroundColor = '#222'
-  document.querySelector('.message').textContent = 'Start guessing...'
+  displayMessage('Start guessing...')
   document.querySelector('.number').style.width = '15rem'
   document.querySelector('.number').textContent = '?'
   document.querySelector('.guess').value = ''
