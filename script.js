e00cc3eaf032a9e76901e673d1f7233781a919eb
harshg28101999 <hmg5265@psu.edu>
2021-06-01 00:30:13 +0530

    Complete working Game logic

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
 'use strict';
 
 // DOM Manipulation
-
-//TODO Work on the logic on the scoring for the player
-//TODO Work on the again button and reset the game
-const number = Math.trunc(Math.random()*20)+1
-console.log(document.querySelector('.number').textContent = number);
+let number = Math.trunc(Math.random()*20)+1
+let score = 20;
+let highScore = 0
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess);
-  console.log(typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!!';
   }
 
+  // When player wins
   else if (guess === number){
     document.querySelector('.message').textContent = 'Correct Guess!';
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+
+    // Implementing our high score functioning
+    if (score > highScore){
+      highScore = score
+      document.querySelector('.highscore').textContent = highScore
+    }
   }
 
+  // When guess is high
   else if (guess > number){
-    document.querySelector('.message').textContent = 'Too High!';
+    if (score > 1){
+      document.querySelector('.message').textContent = 'Too High!';
+      score = score - 1
+      document.querySelector('.score').textContent = score
+    }
+    else {
+      document.querySelector('.message').textContent = 'You Loose!!';
+      document.querySelector('body').style.backgroundColor = '#FF0000'
+      document.querySelector('.score').textContent = 0
+    }
   }
 
+  // When guess is low
   else if (guess < number){
-    document.querySelector('.message').textContent = 'Too Low!';
+    if (score > 0){
+      document.querySelector('.message').textContent = 'Too Low!!';
+      score = score - 1
+      document.querySelector('.score').textContent = score
+    }
+    else {
+      document.querySelector('.message').textContent = 'You Loose!!';
+      document.querySelector('body').style.backgroundColor = '#FF0000'
+      document.querySelector('.score').textContent = 0;
+    }
   }
-
 });
+
+// Using the again button
+document.querySelector('.again').addEventListener('click',function(){
+  
+  score = 20
+  document.querySelector('.score').textContent = score
+  number = Math.trunc(Math.random()*20)+1
+
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.message').textContent = 'Start guessing...'
+  document.querySelector('.number').style.width = '15rem'
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+})
